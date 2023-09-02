3d6406385fc937485468248260e3154d6134fe4d
nce3xin <249424562@qq.com>
2023-09-02 16:23:53 +0800

    stack second verison

---
+++ templates/stack/v2/stack.go
@@ -0,0 +1,34 @@
+package v2
+
+// 数组模拟栈
+
+const N int = 1e5 + 10
+
+// tt表示栈顶，tt=0这个位置不存数据
+var tt int = 0
+var stk []int = make([]int, N)
+
+// push 向栈顶插入一个数
+func push(x int) {
+	tt++
+	stk[tt] = x
+}
+
+// top 从栈顶弹出一个数
+func pop() {
+	tt--
+}
+
+// top 大于等于 0 栈非空，小于 0 栈空
+func empty() bool {
+	if tt > 0 {
+		return false
+	}
+	return true
+}
+
+// 返回栈顶元素
+func getTop() int {
+	x := stk[tt]
+	return x
+}
