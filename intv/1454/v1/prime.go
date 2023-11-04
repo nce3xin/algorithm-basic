b39b67814dd9a2c64507f62dc6440724d1ac5a7f
nce3xin <249424562@qq.com>
2023-11-05 00:06:16 +0800

    异或和是质数的子集数

---
+++ intv/1454/v1/prime.go
@@ -0,0 +1,53 @@
+package main
+
+// 未用滚动数组优化空间复杂度
+
+import "fmt"
+
+const (
+	MOD int = 1e9 + 7
+	N   int = 5010
+	M   int = 8192
+)
+
+var n int
+var a [N]int
+
+// 从前i个数中选且异或和为j的所有方案的集合
+var f [N][M]int
+
+func main() {
+	fmt.Scanf("%d", &n)
+	for i := 1; i <= n; i++ {
+		fmt.Scanf("%d", &a[i])
+	}
+	// 当有0个数的时候，异或和是0，有1个方案. f[0][1], f[0][2]... 都是0
+	f[0][0] = 1
+	for i := 1; i <= n; i++ {
+		for j := 0; j < M; j++ {
+			f[i][j] = f[i-1][j]
+			if j^a[i] < M {
+				f[i][j] = (f[i][j] + f[i-1][j^(a[i])]) % MOD
+			}
+		}
+	}
+	res := 0
+	for i := 2; i < M; i++ {
+		if isPrime(i) {
+			res = (res + f[n][i]) % MOD
+		}
+	}
+	fmt.Printf("%d\n", res)
+}
+
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i <= n/i; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
